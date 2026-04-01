0a3959ce47b63be3dca7293776b8baccee1a0839
LeopoldPichonneau <leopold.beillard@gmail.com>
2026-04-01 11:17:57 +0300

    update

--- utils/pb/suggestions/suggestions_pb2_grpc.py
+++ utils/pb/suggestions/suggestions_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import suggestions_pb2 as suggestions__pb2
 
-GRPC_GENERATED_VERSION = '1.78.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -18,7 +18,7 @@ except ImportError:
 if _version_not_supported:
     raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
-        + ' but the generated code in suggestions_pb2_grpc.py depends on'
+        + f' but the generated code in suggestions_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
